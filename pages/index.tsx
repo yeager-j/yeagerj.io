3b81afec4e77ee1934eea4d870b4d780470b49ec
Jackson Yeager <jacksonyeager@gmail.com>
2022-01-04 00:55:21 -0500

    Added opengraph image

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,14 @@ const Home: NextPage = () => {
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Jackson Yeager" />
                 <meta property="og:description" content="You just rolled a Natural 20 on your Investigation check." />
+                <meta property="og:image" content="https://yeagerj.io/ogimage.png" />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="twitter:domain" content="yeagerj.io" />
                 <meta property="twitter:url" content="https://yeagerj.io/" />
                 <meta name="twitter:title" content="Jackson Yeager" />
                 <meta name="twitter:description" content="You just rolled a Natural 20 on your Investigation check." />
+                <meta name="twitter:image" content="https://yeagerj.io/ogimage.png" />
             </Head>
 
             <Top />
